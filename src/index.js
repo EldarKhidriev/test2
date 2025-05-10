a8ae3689d33fe778900619257f37919d4eaf2637
EldarKhidriev <1mirty1@mail.ru>
2025-05-11 02:49:35 +0300

    fix маски

--- src/index.js
+++ src/index.js
@@ -111,44 +111,11 @@ overlay.addEventListener('click', () => {
 });
 
 
-// Маска для телефона в формате +X (XXX) XXX-XXXX
-document.addEventListener("DOMContentLoaded", function () {
+import Inputmask from "inputmask";
+
+// Маска для телефона: +X (XXX) XXX-XXXX
+document.addEventListener("DOMContentLoaded", () => {
   const phoneInput = document.querySelector("#phone");
-
-  phoneInput.addEventListener("input", function (e) {
-    let inputValue = e.target.value.replace(/\D/g, ""); 
-
-
-    if (inputValue.length > 0 && inputValue[0] !== "+") {
-      inputValue = `+${inputValue}`;
-    }
-
-    if (inputValue.length <= 2) {
-      inputValue = `+${inputValue}`;
-    } else if (inputValue.length <= 5) {
-      inputValue = `+${inputValue.slice(1, 2)} (${inputValue.slice(2)}`;
-    } else if (inputValue.length <= 8) {
-      inputValue = `+${inputValue.slice(1, 2)} (${inputValue.slice(2, 5)}) ${inputValue.slice(5)}`;
-    } else if (inputValue.length <= 12) {
-      inputValue = `+${inputValue.slice(1, 2)} (${inputValue.slice(2, 5)}) ${inputValue.slice(5, 8)}-${inputValue.slice(8)}`;
-    } else {
-      inputValue = `+${inputValue.slice(1, 2)} (${inputValue.slice(2, 5)}) ${inputValue.slice(5, 8)}-${inputValue.slice(8, 12)}-${inputValue.slice(12, 15)}`;
-    }
-
-
-    if (inputValue.length > 16) {
-      inputValue = inputValue.slice(0, 17);
-    }
-
-    e.target.value = inputValue; 
-  });
-
-
-  const contactForm = document.querySelector("#contactForm");
-  const submitButton = document.querySelector("#submitBtn");
-
-  contactForm.addEventListener("input", function () {
-    const isValid = contactForm.checkValidity();
-    submitButton.disabled = !isValid;
-  });
+  const im = new Inputmask("+9 (999) 999-9999");
+  im.mask(phoneInput);
 });
